f74b1eb198c5b748518ec8f7b8bfb5a856d5ea7e
MaclaRamirez <maclarata@gmail.com>
2013-06-13 13:27:32 +0200

    Change '=' by deepcopy(), and add absolute value to the result, so it give us the actual difference

--- example_2.py
+++ example_2.py
@@ -4,14 +4,17 @@
 # the original list of values, and the new list of values. This should give
 # 0.625 but it gives 0.0 instead. Why is this?
 
+#This is why with lists, when we say b = a, we point b to the same location as
+#a, so the list exist only once in the memory. We have to do a deepcopy.
 
 import numpy as np
+from copy import deepcopy
 
 # Define the original set of values.
-values = [8., 7., 9., 4., 6., 7., 8., 4.]
+new_values = [8., 7., 9., 4., 6., 7., 8., 4.]
 
 # Create a new list with the original values and add a few more
-new_values = values
+values = deepcopy(new_values)
 new_values.append(8.)
 new_values.append(9.)
 new_values.append(2.)
@@ -20,4 +23,4 @@ new_values.append(5.)
 
 print values
 print new_values
-print "Difference in the mean:", np.mean(new_values) - np.mean(values)
+print "Difference in the mean:", abs(np.mean(new_values) - np.mean(values))
